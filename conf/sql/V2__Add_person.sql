3ef4cc943899c329aa517cd98167272093c89e0d
Said Dinar Fadilah <saiddinarfadilah@gmail.com>
2024-10-27 16:08:08 +0700

    ci(file): add Jenkinsfile-2.groovy-V2__Add_person.sql

--- conf/sql/V2__Add_person.sql
+++ conf/sql/V2__Add_person.sql
@@ -1,3 +1,3 @@
-INSERT INTO person (id,name) VALUES ("1", "Asep");
-INSERT INTO person (id,name) VALUES ("2", "Jajang");
-INSERT INTO person (id,name) VALUES ("3", "Nanang");
+INSERT INTO person (id,name) VALUES ('1', 'Asep');
+INSERT INTO person (id,name) VALUES ('2', 'Jajang');
+INSERT INTO person (id,name) VALUES ('3', 'Nanang');
